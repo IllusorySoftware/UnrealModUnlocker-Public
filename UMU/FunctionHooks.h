9d296d476aac90635f36e3ac1e37bd306630e8da
GHFear <32522043+GHFear@users.noreply.github.com>
2022-04-21 14:34:27 +0200

    Update FunctionHooks.h
    Fixed FindFileInPakFile Hook Version 2 so it works with Dragon Quest XI S and other games using the same engine version.

--- UMU/FunctionHooks.h
+++ UMU/FunctionHooks.h
@@ -7,10 +7,7 @@
 
 namespace FunctionHooks
 {
-	bool DoesFileExist(LPCWSTR path);
-	void* __fastcall FindFileInPakFiles_Hook(void* PakFile, void* Filename, void* FileEntry);
-	void* __fastcall FindFileInPakFiles_2_Hook(void* PakFile, void* Filename, void** FileEntry);
-	__int64 __fastcall IsNonPakFilenameAllowed_Hook(void* thisptr, void* InFilename);
+
 	const wchar_t* GameStart = L"../../../"; // This is at the start of every game path
 
 	//Check if file exists at path
@@ -26,13 +23,13 @@ namespace FunctionHooks
 	FindFileInPakFiles_ptr FindFileInPakFiles_orig;
 
 	// PakFile::Find hook: this will check for any loose file with the same filename, and if a loose file is found will return false (ie: saying that the .pak doesn't contain it)
-	// FindFileInPakFiles 4.12 - 4.27
+	// FindFileInPakFiles Version 1
 	void* __fastcall FindFileInPakFiles_Hook(void* PakFile, void* Filename, void* FileEntry)
 	{
 
 		const TCHAR* fname = *(TCHAR**)Filename;
 
-		if (wcsstr(fname, GameStart) && DoesFileExist(fname)) {
+		if (Filename && wcsstr(fname, GameStart) && DoesFileExist(fname)) {
 
 
 			return 0; // The file exists outside the PAK and, return false here so the game believes that it doesn't exists in the PAK.
@@ -48,24 +45,22 @@ namespace FunctionHooks
 	typedef void* (*FindFileInPakFiles_2_ptr)(void* PakFile, void* Filename, void** FileEntry);
 	FindFileInPakFiles_2_ptr FindFileInPakFiles_2_orig;
 
-
-	void* __fastcall FindFileInPakFiles_2_Hook(void* PakFile, void* Filename, void** FileEntry)
+	// PakFile::Find hook: this will check for any loose file with the same filename, and if a loose file is found will return false (ie: saying that the .pak doesn't contain it)
+	// FindFileInPakFiles Version 2
+	void* __fastcall FindFileInPakFiles_2_Hook(void* PakFile, TCHAR* Filename, void** FileEntry)
 	{
 
 		if (FileEntry)
 		{
 			*FileEntry = nullptr;
 		}
+			
 
-
-		TCHAR* fname = *(TCHAR**)Filename;
-
-
-		if (Filename && wcsstr(fname, GameStart) && DoesFileExist(fname))
+		if (Filename && wcsstr(Filename, GameStart) && DoesFileExist(Filename))
 		{
-
 			return 0; // The file exists outside the PAK and, return false here so the game believes that it doesn't exists in the PAK.
 		}
+			
 
 		return FindFileInPakFiles_2_orig(PakFile, Filename, FileEntry);
 	}
